9905b73b684ce8a9de8bfa1c68ee3384e35014c6
Niko <bow682@gmail.com>
2021-12-01 12:33:06 -0500

    BIG progress

--- live/bgg_queries.js
+++ live/bgg_queries.js
@@ -1,17 +1,27 @@
 const [UNSENT, OPENED, HEADERS_RECIEVED, LOADING, DONE] = [0, 1, 2, 3, 4];
 
+const id = id => document.getElementById(id);
+
 const game_query = (id) => `https://api.geekdo.com/xmlapi2/thing?id=${id}`;
-const user_link = 'https://api.geekdo.com/xmlapi2/collection?username=GreatOaksTavern';
+const user_link  = 'https://api.geekdo.com/xmlapi2/collection?username=GreatOaksTavern';
+var query_list;
+var current_item;
+var interval;
+var index = 0;
+var item_count = 0;
+
+
 
 function update_database() {
+
     //get our list of ids from firestore
     const listRef = db.collection('games').doc("ID_LIST");
-
+    
     listRef.get().then((doc) => {
         if (doc.exists) {
             const current_list = doc.data().list;
 
-            //request updated list from bgg, compare to firestore list
+            //request updated list from bgg
             
             const xhr = new XMLHttpRequest();
 
@@ -21,29 +31,50 @@ function update_database() {
                         switch (xhr.status) {
                             case 200: //successful response
                                 const { serverTimestamp } = firebase.firestore.FieldValue;
-                                const parser = new DOMParser();
-                                const xmlDOM = parser.parseFromString(xhr.responseText, 'application/xml');
+                                let parser = new DOMParser();
+                                let xmlDOM = parser.parseFromString(xhr.responseText, 'application/xml');
                                 const gamesList = xmlDOM.querySelectorAll('item');
-                                let id_list = [];
+                                let new_list = [];
                                 let owned;
 
+                                //once we have response xml, boil it down into an array of ids
+
                                 gamesList.forEach(game => {
-                                    owned = game.querySelector('status').attributes.own.nodeValue;  //xml querying :')
-                                    if (owned == 1) id_list.push(game.attributes.objectid.nodeValue);
+                                    // making sure we only add ids of games marked as 'owned'
+                                    
+                                    owned = game.querySelector('status').attributes.own.nodeValue;
+                                    
+                                    if (owned == 1) {
+                                        new_list.push(game.attributes.objectid.nodeValue);
+                                    }
                                 });
+                                
+                                //compare to firestore list
+
+                                let same = arrayEquals(new_list,current_list);
 
                                 //if no changes, exit early
 
+                                if (same) {
+                                    console.log('Values are the same. No need to update');
+                                    //return;
+                                }
+
                                 //otherwise, filter down list to only what is new
 
-                                //update our id_list
+                                //update our current_list
 
                                 //listRef.update({
-                                    //list: id_list,
+                                    //list: new_list,
                                     //lastUpdate: serverTimestamp()
                                 //});
 
                                 //query each item in 10 second intervals, add info to firestore
+
+                                query_list  = new_list;
+                                item_count  = query_list.length;
+                                interval    = setInterval(db_update_step, 10000);
+                                db_update_step();
 
                                 break;
                             case 404: //unsuccessful response
@@ -64,4 +95,59 @@ function update_database() {
     }).catch((error) => {
         console.log("Error getting document:", error);
     });
+}
+
+function db_update_step(){
+    
+    if (query_list.length <= 0) {
+        id('update-info').innerText = "Done!";
+        clearInterval(interval);
+        return 1;
+    }
+
+    current_item = query_list.pop();
+    id('update-info').innerText = 'Making request...';
+    fetch(game_query(current_item))
+        .then(response => response.text())
+        .then(data => {
+            let parser                  = new DOMParser();
+            let xmlDOM                  = parser.parseFromString(data, 'application/xml');
+            let game                    = xmlDOM.querySelector('item');
+            let name                    = game.querySelector('name').attributes.value.nodeValue;
+            let year                    = game.querySelector('yearpublished').attributes.value.nodeValue;
+            let minplayers              = game.querySelector('minplayers').attributes.value.nodeValue;
+            let maxplayers              = game.querySelector('maxplayers').attributes.value.nodeValue;
+            let playingtime             = game.querySelector('playingtime').attributes.value.nodeValue;
+            let minage                  = game.querySelector('minage').attributes.value.nodeValue;
+            let image                   = game.querySelector('image').innerHTML;
+            let thumbnail               = game.querySelector('thumbnail').innerHTML;
+            
+            index++;
+            id('update-info').innerText = 
+            `Getting info for item ${index.toString()} of ${item_count.toString()}... 
+            name: ${name}
+            BGG id: ${current_item}`;
+            
+            id('game-img').src = thumbnail;
+
+            db.collection("games").doc(current_item).set({
+                name,
+                image,
+                thumbnail,
+                year,
+                minplayers,
+                maxplayers,
+                playingtime,
+                minage
+            })
+        })
+}
+
+function arrayEquals(a, b) {
+    return (
+        Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index])
+    );
 }
